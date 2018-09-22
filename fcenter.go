1a841d4a6dff64905d6aafd1cd7d48738f7a36c3
kraser <kravalsergey@yandex.ru>
2018-09-22 22:46:05 +0700

    0.0.2

--- fcenter.go
+++ fcenter.go
@@ -8,6 +8,8 @@ import (
 
 	"fmt"
 	"os"
+
+	"os/signal"
 	"priceloader"
 	"strings"
 	"webreader"
@@ -18,6 +20,9 @@ import (
 const (
 	SUPPLIER_CODE string = "mvideo"
 	URL           string = "http://fcenter.ru"
+	ENDMESSAGE    string = "ItemsLoaded"
+	WORKERS       int    = 5
+	WORKERSCAP    int    = 5
 )
 
 var (
@@ -29,12 +34,12 @@ func init() {
 	flag.StringVar(&logMode, "lm", logMode, "режим логгирования")
 	flag.StringVar(&city, "city", logMode, "город для которого разбирается прайс")
 
-	flag.Parse()
 	logMode = "debug"
 	logger.SetLogLevel(logMode)
 }
 
 func initParser() {
+	flag.Parse()
 	parser := parsers.GetParser()
 	parser.Options.Url = URL
 	parser.Options.AddHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
@@ -63,14 +68,14 @@ func main() {
 	dom, err := goquery.NewDocumentFromReader(strings.NewReader(result))
 	errorHandle(err)
 
-	dom.Find("#bottomCatalog")
+	//dom.Find("#bottomCatalog")
 	catalog := dom.Find("#bottomCatalog").First()
 	columns := catalog.Find(".category-data")
 	for i := range columns.Nodes {
 		subCategoriesNodes := columns.Eq(i)
 		categoryName := strings.TrimSpace(subCategoriesNodes.Find(".category-name").Text())
 		fmt.Println("LEVEL0: ", categoryName)
-		category := priceloader.PriceList.SetCurrentCategory(categoryName, 0)
+		category := priceloader.PriceList.SetCurrentCategory(categoryName, "", 0)
 		fmt.Println("CREATED0", *category)
 
 		anchors := subCategoriesNodes.Find("a")
@@ -78,27 +83,103 @@ func main() {
 			subCategoryName := s.Text()
 			link, _ := s.Attr("href")
 			fmt.Println("LEVEL1", subCategoryName, link)
-			subCategory := priceloader.PriceList.SetCurrentCategory(subCategoryName, 1)
-			subCategory.URL = link
+			subCategory := priceloader.PriceList.SetCurrentCategory(subCategoryName, link, 1)
 			fmt.Println("CREATED1", *subCategory)
 		})
 
 	}
-	checkCategoriesStructure()
+	loadItems()
 }
 
+/*
 func checkCategoriesStructure() {
 	pPriceList := priceloader.PriceList
 	for name, value := range pPriceList.Categories {
-		fmt.Println(name, len(value.Categories))
+		fmt.Println(name, value.URL, *value)
 		for name1, value1 := range value.Categories {
-			fmt.Println("  ", name1, *value1)
+			fmt.Println("  ", name1, value1.URL, *value1)
 		}
 	}
 }
-
+*/
 func errorHandle(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+func loadItems() {
+	//Подготовим каналы и балансировщик
+	linksChan := make(chan string)
+	quitChan := make(chan bool)
+	pBalancer := new(Balancer)
+	pBalancer.init(linksChan)
+
+	//Подготовим каналы и балансировщик
+	//links := make(chan string)
+	//quit := make(chan bool)
+	//b := new(Balancer)
+	//b.init(links)
+
+	//Приготовимся перехватывать сигнал останова в канал keys
+	keys := make(chan os.Signal, 1)
+	signal.Notify(keys, os.Interrupt)
+
+	//Запускаем балансировщик и генератор
+	go pBalancer.balance(quitChan)
+	go generator(linksChan)
+
+	fmt.Println("Начинаем загрузку изображений...")
+	//Основной цикл программы:
+	for {
+		select {
+		case <-keys: //пришла информация от нотификатора сигналов:
+			fmt.Println("CTRL-C: Ожидаю завершения активных загрузок")
+			quitChan <- true //посылаем сигнал останова балансировщику
+
+		case <-quitChan: //пришло подтверждение о завершении от балансировщика
+			fmt.Println("Загрузки завершены!")
+			return
+		}
+	}
+}
+
+func generator(out chan string) {
+	pPriceList := priceloader.PriceList
+	for name, value := range pPriceList.Categories {
+		fmt.Println(name, value.URL, *value)
+		for _, subCat := range value.Categories {
+			fmt.Println("  ", subCat.Name, subCat.URL)
+			//}
+
+			//for pos := start; ; pos += 20 {
+			//Разбираем страницу:
+			//x, err := goquery.ParseUrl("http://home.atata.com/streams/" + strconv.Itoa(stream) + "?order=date&from=" + strconv.Itoa(pos))
+			//if err == nil {
+			//Отправляем все найденные ссылки в поток:
+			//for _, url := range x.Find("figure a.image").Attrs("href") {
+			out <- URL + subCat.URL
+		}
+		//А если встретили признак последней страницы - отправляем кодовую фразу..
+		/*
+			if len(x.Find("li.last.hide")) > 0 {
+				out <- ENDMESSAGE
+				//..и прекращаем работу генератора
+				return
+			}
+		*/
+		//}
+	}
+	out <- ENDMESSAGE
+}
+
+func getItemHtml(pageUrl string) {
+	parser := parsers.GetParser()
+	result := webreader.DoRequest(pageUrl, parser.Options)
+	fileName := "/home/robot/" + pageUrl[strings.LastIndex(pageUrl, "/")+1:]
+	fileHandler, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
+	errorHandle(err)
+	defer fileHandler.Close()
+	fileHandler.Truncate(0)
+	fileHandler.WriteString(result)
 }
---
+++ worker.go
@@ -0,0 +1,170 @@
+// worker
+package main
+
+import (
+	"container/heap"
+	//"fmt"
+	"sync"
+)
+
+//Рабочий
+type Worker struct {
+	urls    chan string     // канал для заданий
+	pending int             // кол-во оставшихся задач
+	index   int             // позиция в куче
+	wg      *sync.WaitGroup //указатель на группу ожидания
+}
+
+func (w *Worker) work(done chan *Worker) {
+	for {
+		url := <-w.urls  //читаем следующее задание
+		w.wg.Add(1)      //инкриминируем счетчик группы ожидания
+		getItemHtml(url) //загружаем файл
+		w.wg.Done()      //сигнализируем группе ожидания что закончили
+		done <- w        //показываем что завершили работу
+	}
+}
+
+//Это будет наша "куча":
+type Pool []*Worker
+
+//Проверка кто меньше - в нашем случае меньше тот у кого меньше заданий:
+func (p Pool) Less(i, j int) bool { return p[i].pending < p[j].pending }
+
+//Вернем количество рабочих в пуле:
+func (p Pool) Len() int { return len(p) }
+
+//Реализуем обмен местами:
+func (p Pool) Swap(i, j int) {
+	if i >= 0 && i < len(p) && j >= 0 && j < len(p) {
+		p[i], p[j] = p[j], p[i]
+		p[i].index, p[j].index = i, j
+	}
+}
+
+//Заталкивание элемента:
+func (p *Pool) Push(x interface{}) {
+	n := len(*p)
+	worker := x.(*Worker)
+	worker.index = n
+	*p = append(*p, worker)
+}
+
+//И выталкивание:
+func (p *Pool) Pop() interface{} {
+	old := *p
+	n := len(old)
+	item := old[n-1]
+	item.index = -1
+	*p = old[0 : n-1]
+	return item
+}
+
+//Балансировщик
+type Balancer struct {
+	pool     Pool            //Наша "куча" рабочих
+	done     chan *Worker    //Канал уведомления о завершении для рабочих
+	requests chan string     //Канал для получения новых заданий
+	flowctrl chan bool       //Канал для PMFC
+	queue    int             //Количество незавершенных заданий переданных рабочим
+	wg       *sync.WaitGroup //Группа ожидания для рабочих
+}
+
+//Инициализируем балансировщик. Аргументом получаем канал по которому приходят задания
+func (b *Balancer) init(in chan string) {
+	b.requests = make(chan string)
+	b.flowctrl = make(chan bool)
+	b.done = make(chan *Worker)
+	b.wg = new(sync.WaitGroup)
+
+	//Запускаем наш Flow Control:
+	go func() {
+		for {
+			b.requests <- <-in //получаем новое задание и пересылаем его на внутренний канал
+			<-b.flowctrl       //а потом ждем получения подтверждения
+		}
+	}()
+
+	//Инициализируем кучу и создаем рабочих:
+	heap.Init(&b.pool)
+	for i := 0; i < WORKERS; i++ {
+		w := &Worker{
+			urls:    make(chan string, WORKERSCAP),
+			index:   0,
+			pending: 0,
+			wg:      b.wg,
+		}
+		go w.work(b.done)     //запускаем рабочего
+		heap.Push(&b.pool, w) //и заталкиваем его в кучу
+	}
+}
+
+//Рабочая функция балансировщика получает аргументом канал уведомлений от главного цикла
+func (b *Balancer) balance(quit chan bool) {
+	lastjobs := false //Флаг завершения, поднимаем когда кончились задания
+	for {
+		select { //В цикле ожидаем коммуникации по каналам:
+
+		case <-quit: //пришло указание на остановку работы
+			b.wg.Wait()  //ждем завершения текущих загрузок рабочими..
+			quit <- true //..и отправляем сигнал что закончили
+
+		case url := <-b.requests: //Получено новое задание (от flow controller)
+			if url != ENDMESSAGE { //Проверяем - а не кодовая ли это фраза?
+				b.dispatch(url) // если нет, то отправляем рабочим
+			} else {
+				lastjobs = true //иначе поднимаем флаг завершения
+			}
+
+		case w := <-b.done: //пришло уведомление, что рабочий закончил загрузку
+			b.completed(w) //обновляем его данные
+			if lastjobs {
+				if w.pending == 0 { //если у рабочего кончились задания..
+					heap.Remove(&b.pool, w.index) //то удаляем его из кучи
+				}
+				if len(b.pool) == 0 { //а если куча стала пуста
+					//значит все рабочие закончили свои очереди
+					quit <- true //и можно отправлять сигнал подтверждения готовности к останову
+				}
+			}
+		}
+	}
+}
+
+// Функция отправки задания
+func (b *Balancer) dispatch(url string) {
+	w := heap.Pop(&b.pool).(*Worker) //Берем из кучи самого незагруженного рабочего..
+	w.urls <- url                    //..и отправляем ему задание.
+	w.pending++                      //Добавляем ему "весу"..
+	heap.Push(&b.pool, w)            //..и отправляем назад в кучу
+	if b.queue++; b.queue < WORKERS*WORKERSCAP {
+		b.flowctrl <- true
+	}
+}
+
+//Обработка завершения задания
+func (b *Balancer) completed(w *Worker) {
+	w.pending--
+	heap.Remove(&b.pool, w.index)
+	heap.Push(&b.pool, w)
+	if b.queue--; b.queue == WORKERS*WORKERSCAP-1 {
+		b.flowctrl <- true
+	}
+}
+
+/*
+//Загрузка изображения
+func download(url string) {
+	fileName := IMGDIR + "/" + url[strings.LastIndex(url, "/")+1:]
+	output, err := os.Create(fileName)
+	defer output.Close()
+
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error while downloading", url, "-", err)
+		return
+	}
+	defer response.Body.Close()
+	io.Copy(output, response.Body)
+}
+*/
